70e49cba1f42b6662e9b1221633f57c7f6bb0d55
Mariano Montero <monterosmarianos@hotmail.com.ar>
2021-06-15 17:29:03 -0300

    issue solved

--- Pagina/CentroAcopio/templates/PedidoDeRetiro.tpl
+++ Pagina/CentroAcopio/templates/PedidoDeRetiro.tpl
@@ -2,18 +2,20 @@
 {include file="navCiudadano.tpl"}
     <div class="contact-clean transparente">
         <form action="agregarPedido" method="post" enctype="multipart/form-data">
-            <h2 class="text-center">Formulario de ciudadano</h2>
+            <h2 class="text-center">Formulario de Pedido de Retiro</h2>
             <div class=" hidden"><input class="form-control" type="radio" value="11254" checked="checked" class="hidden" name="id"></div>
             <div class="form-group form-horario"><p>volumen</p>
-            <div class="form-group"><label for=""><input type="radio" checked="checked" value="1" class="radio-horario" type="text" name="volumen">menos de 1 metro²</label></div>
-            <div class="form-group"><label for=""><input type="radio" value="2" class="radio-horario" type="text" name="volumen">entre 1 y 2 metros²</label></div>
-            <div class="form-group"><label for=""><input type="radio" value="3" class="radio-horario" type="text" name="volumen">entre 2 y 4 metros²</label></div>
-            <div class="form-group"><label for=""><input type="radio" value="4" class="radio-horario" type="text" name="volumen">mas de 4 metros²</label></div>
+            <div class="form-group"><label for=""><input type="radio" checked="checked" value="1" class="radio-horario" type="text" name="volumen">Entra en una caja</label></div>
+            <div class="form-group"><label for=""><input type="radio" value="2" class="radio-horario" type="text" name="volumen">Entra en el baúl de un auto</label></div>
+            <div class="form-group"><label for=""><input type="radio" value="3" class="radio-horario" type="text" name="volumen">Entra en la caja de una camioneta</label></div>
+            <div class="form-group"><label for=""><input type="radio" value="4" class="radio-horario" type="text" name="volumen">Es necesario un camión</label></div>
             <form class="form-group" method="post" action="addImages/{$products->id_product}" enctype="multipart/form-data" id="imagesToUpload">
                 <input type="file" name="image" id="imageToUpload" multiple>
                 <button type="submit" class="btn btn-primary ">Add</button>
             </form>
-            <div class="form-group"><button class="btn btn-primary" type="submit" style="background-color: rgb(5,218,116);">Enviar </button></div>
+            <div class="form-group"><button class="btn btn-primary" type="submit" style="background-color: rgb(5,218,116);">Enviar</button></div>
         </form>
     </div>
+    <br>
+    <br>
 {include file="footer.tpl"}
--- Pagina/CentroAcopio/templates_c/758ff722f40627b0c26b2bbf349dcd0e568e9ad3_0.file.PedidoDeRetiro.tpl.php
+++ Pagina/CentroAcopio/templates_c/758ff722f40627b0c26b2bbf349dcd0e568e9ad3_0.file.PedidoDeRetiro.tpl.php
@@ -1,85 +1,51 @@
 <?php
-/* Smarty version 3.1.33, created on 2021-05-25 23:18:10
+/* Smarty version 3.1.33, created on 2021-06-15 22:27:01
   from 'C:\xampp\htdocs\Proyectos\MetodologiasGrupo12\Pagina\CentroAcopio\templates\PedidoDeRetiro.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_60ad69926a2132_77411853',
+  'unifunc' => 'content_60c90d158fc580_23278050',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '758ff722f40627b0c26b2bbf349dcd0e568e9ad3' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Proyectos\\MetodologiasGrupo12\\Pagina\\CentroAcopio\\templates\\PedidoDeRetiro.tpl',
-      1 => 1621977483,
+      1 => 1623788819,
       2 => 'file',
     ),
   ),
   'includes' => 
   array (
+    'file:header.tpl' => 1,
+    'file:navCiudadano.tpl' => 1,
+    'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_60ad69926a2132_77411853 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60c90d158fc580_23278050 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+$_smarty_tpl->_subTemplateRender("file:navCiudadano.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Registro Ciudadano</title>
-    <link rel="stylesheet" href="css/css.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-</head>
-
-<body>
-    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
-        <div class="container"><a class="navbar-brand logo" href="#">Cooperativa de Recuperadores Urbanos de Tandil<br></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div
-                class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">Home</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="Login.html">Login</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="EleccionReg.html">Registrarme</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="Ofertas.html">Ofertas</a></li>
-                </ul>
-        </div>
-        </div>
-    </nav>
-    <div class="contact-clean fondo">
-        <form action="agregarPedido" method="post">
-            <h2 class="text-center">Formulario de ciudadano</h2>
+    <div class="contact-clean transparente">
+        <form action="agregarPedido" method="post" enctype="multipart/form-data">
+            <h2 class="text-center">Formulario de Pedido de Retiro</h2>
             <div class=" hidden"><input class="form-control" type="radio" value="11254" checked="checked" class="hidden" name="id"></div>
-            <div class="form-group form-horario"><p>Tamanio</p>
-            <div class="form-group"><label for=""><input type="radio" checked="checked" value="1" class="radio-horario" type="text" name="volumen">menos de 1 metro²</label></div>
-            <div class="form-group"><label for=""><input type="radio" value="2" class="radio-horario" type="text" name="volumen">entre 1 y 2 metros²</label></div>
-            <div class="form-group"><label for=""><input type="radio" value="3" class="radio-horario" type="text" name="volumen">entre 2 y 4 metros²</label></div>
-            <div class="form-group"><label for=""><input type="radio" value="4" class="radio-horario" type="text" name="volumen">mas de 4 metros²</label></div>
-            </div>
-            <div class="form-group"><input type="file" id="files" name="imagenes" multiple></div>
-            <div class="form-group"><button class="btn btn-primary" type="submit" style="background-color: rgb(5,218,116);">Enviar </button></div>
+            <div class="form-group form-horario"><p>volumen</p>
+            <div class="form-group"><label for=""><input type="radio" checked="checked" value="1" class="radio-horario" type="text" name="volumen">Entra en una caja</label></div>
+            <div class="form-group"><label for=""><input type="radio" value="2" class="radio-horario" type="text" name="volumen">Entra en el baúl de un auto</label></div>
+            <div class="form-group"><label for=""><input type="radio" value="3" class="radio-horario" type="text" name="volumen">Entra en la caja de una camioneta</label></div>
+            <div class="form-group"><label for=""><input type="radio" value="4" class="radio-horario" type="text" name="volumen">Es necesario un camión</label></div>
+            <form class="form-group" method="post" action="addImages/<?php echo $_smarty_tpl->tpl_vars['products']->value->id_product;?>
+" enctype="multipart/form-data" id="imagesToUpload">
+                <input type="file" name="image" id="imageToUpload" multiple>
+                <button type="submit" class="btn btn-primary ">Add</button>
+            </form>
+            <div class="form-group"><button class="btn btn-primary" type="submit" style="background-color: rgb(5,218,116);">Enviar</button></div>
         </form>
     </div>
-    <main class="page landing-page"></main>
-    <?php echo '<script'; ?>
- src="assets/js/jquery.min.js"><?php echo '</script'; ?>
->
-    <?php echo '<script'; ?>
- src="assets/bootstrap/js/bootstrap.min.js"><?php echo '</script'; ?>
->
-    <?php echo '<script'; ?>
- src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"><?php echo '</script'; ?>
->
-    <?php echo '<script'; ?>
- src="assets/js/smoothproducts.min.js"><?php echo '</script'; ?>
->
-    <?php echo '<script'; ?>
- src="assets/js/theme.js"><?php echo '</script'; ?>
->
-</body>
-
-</html><?php }
+    <br>
+    <br>
+<?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
 }
