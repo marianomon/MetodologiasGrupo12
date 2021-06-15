1b69c70b02cc463dadb123cce0b8774fc11331c7
IvanSamudio <39971544+IvanSamudio@users.noreply.github.com>
2021-06-15 17:51:54 -0300

    Terminado todo funcionando

--- Pagina/CentroAcopio/templates/PedidoDeRetiro.tpl
+++ Pagina/CentroAcopio/templates/PedidoDeRetiro.tpl
@@ -4,7 +4,7 @@
         <form action="agregarPedido" method="post" enctype="multipart/form-data">
             <h2 class="text-center">Formulario de Pedido de Retiro</h2>
             <div class=" hidden"><input class="form-control" type="radio" value="11254" checked="checked" class="hidden" name="id"></div>
-            <div class="form-group form-horario"><p>volumen</p>
+            <div class="form-group form-horario"><p>Los materiales entran en:</p>
             <div class="form-group"><label for=""><input type="radio" checked="checked" value="1" class="radio-horario" type="text" name="volumen">Entra en una caja</label></div>
             <div class="form-group"><label for=""><input type="radio" value="2" class="radio-horario" type="text" name="volumen">Entra en el baúl de un auto</label></div>
             <div class="form-group"><label for=""><input type="radio" value="3" class="radio-horario" type="text" name="volumen">Entra en la caja de una camioneta</label></div>
@@ -16,6 +16,6 @@
             <div class="form-group"><button class="btn btn-primary" type="submit" style="background-color: rgb(5,218,116);">Enviar</button></div>
         </form>
     </div>
-    <br>
-    <br>
+</div>
+
 {include file="footer.tpl"}
--- Pagina/CentroAcopio/templates_c/38f9d995437a31c681499b89b2acfae6f0a2186c_0.file.navciudadano.tpl.php
+++ Pagina/CentroAcopio/templates_c/38f9d995437a31c681499b89b2acfae6f0a2186c_0.file.navciudadano.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2021-06-15 22:04:46
+/* Smarty version 3.1.33, created on 2021-06-15 22:47:04
   from 'C:\xampp\htdocs\proyectos\MetodologiasGrupo12\Pagina\CentroAcopio\templates\navciudadano.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_60c907dea2b559_74059673',
+  'unifunc' => 'content_60c911c842b082_71488321',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '38f9d995437a31c681499b89b2acfae6f0a2186c' => 
     array (
       0 => 'C:\\xampp\\htdocs\\proyectos\\MetodologiasGrupo12\\Pagina\\CentroAcopio\\templates\\navciudadano.tpl',
-      1 => 1623786364,
+      1 => 1623790021,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_60c907dea2b559_74059673 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60c911c842b082_71488321 (Smarty_Internal_Template $_smarty_tpl) {
 ?>    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
         <div class="container"><a class="navbar-brand logo" href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 ">Cooperativa de Recuperadores Urbanos de Tandil</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
@@ -28,7 +28,7 @@ function content_60c907dea2b559_74059673 (Smarty_Internal_Template $_smarty_tpl)
                 id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item" role="presentation"><a class="nav-link" href="generarPedidoRetiro">Retiros</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="pedidos">Ofertas</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link disable" href="#">Ofertas</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="LogOut">Salir</a></li>
                 </ul>
             </div>
