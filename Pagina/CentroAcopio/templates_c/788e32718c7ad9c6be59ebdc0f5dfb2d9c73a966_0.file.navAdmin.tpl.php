1e13e7f65f8b95ae762fb7e0307085198fd347ae
IvanSamudio <39971544+IvanSamudio@users.noreply.github.com>
2021-06-16 14:57:26 -0300

    Video de funcionalidad agregado

--- Pagina/CentroAcopio/templates_c/788e32718c7ad9c6be59ebdc0f5dfb2d9c73a966_0.file.navAdmin.tpl.php
+++ Pagina/CentroAcopio/templates_c/788e32718c7ad9c6be59ebdc0f5dfb2d9c73a966_0.file.navAdmin.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2021-06-15 22:15:48
+/* Smarty version 3.1.33, created on 2021-06-16 19:45:09
   from 'C:\xampp\htdocs\proyectos\MetodologiasGrupo12\Pagina\CentroAcopio\templates\navAdmin.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_60c90a74d7b944_87099736',
+  'unifunc' => 'content_60ca38a5a18434_57660147',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '788e32718c7ad9c6be59ebdc0f5dfb2d9c73a966' => 
     array (
       0 => 'C:\\xampp\\htdocs\\proyectos\\MetodologiasGrupo12\\Pagina\\CentroAcopio\\templates\\navAdmin.tpl',
-      1 => 1623788120,
+      1 => 1623819978,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_60c90a74d7b944_87099736 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60ca38a5a18434_57660147 (Smarty_Internal_Template $_smarty_tpl) {
 ?>    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
         <div class="container"><a class="navbar-brand logo" id="navAdminLogo" href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
 ">Cooperativa de Recuperadores Urbanos de Tandil</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
